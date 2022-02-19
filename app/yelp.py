f47ea36a1614f5f46885bef316947267b6f26bdd
Carlos Esteban <estebcar29@gmail.com>
2022-02-19 01:19:25 -0600

    added hello world view and url

--- app/yelp.py
+++ app/yelp.py
@@ -2,26 +2,33 @@
 import requests
 from decouple import config
 
-#Define a business ID
-#business_id
-
 #Define the API Key, Endpoint, and Header
 API_KEY = config("API_KEY")
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 HEADERS = {'Authorization': 'bearer %s' % API_KEY}
 
-#Define the parameters
-PARAMETERS = {'term':'coffee',
-              'limit': 50,
-              'radius': 40000,
-              'location':'1000 McKenzie Dr, Georgetown, TX 78626'}
 
-#Make a request to the yelp API
-response = requests.get(url = ENDPOINT, params= PARAMETERS, headers = HEADERS)
+def restaurants_in_radius(latitude,longitude,radius,limit,offset):
+  #defines the parameters
+  PARAMS = {'term': 'restaurants',
+            'latitude': latitude,
+            'longitude': longitude,
+            'radius': radius,
+            'limit': 50,
+            'offset': offset}
+  #makes a request to the YELP Api
+  response = requests.get(url = ENDPOINT, params = PARAMS, headers = HEADERS)
+  #Converts JSON string to a dictionary
+  data = response.json()
+  #prints the name of each restaurant
+  for res in data['businesses']:
+    print(res['name'])
+  #print(data)
+  #calls function again if the limit is not reached
+  if(limit - offset != 50):
+    restaurants_in_radius(latitude,longitude,radius,limit,offset+50)
 
-#Convert JSON string to a Dictionary
-business_data = response.json()
-
-for biz in business_data['businesses']:
-  print(biz['name'])
-
+#example function call using lat and long of Austin,TX    
+restaurants_in_radius(30.267153,-97.743057,16093,200,0)
+  
+  
