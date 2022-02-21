179dd95657d58da2ce5c4a60c17f9cfa1033b36d
Tiannajones <77419054+Tiannajones@users.noreply.github.com>
2022-02-21 11:11:59 -0600

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Admin_site -- used this link to get from .models and to register models 
+# this code imports the models and then calls admin.site.register to register them
+
 from django.contrib import admin
+from .models import User, restUser
 
 # Register your models here.
+admin.site.register(User)    # for basic user 
+admin.site.register(restUser)  # for restaurant owner and user 
