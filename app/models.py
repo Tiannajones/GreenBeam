15cce031dada62d699a052182f5fe4c81d29102e
Carlos Esteban <estebcar29@gmail.com>
2022-03-06 11:08:33 -0600

    added restaurant models

--- app/models.py
+++ app/models.py
@@ -21,3 +21,22 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+#Create table for the restaurant data or name Restuarants
+class YelpOwned(models.Model):
+    business_id = models.PositiveBigIntegerField(primary_key=1)
+    name = models.TextField
+    description = models.TextField
+    yelp_rating = models.TextField
+    phone_number = models.IntegerField(max_length=10)
+    price = models.IntegerField(max_length=3)
+    latitude = #figure out specific field
+    longitude = #figure out specific field
+    address = #figure out specific address and probably break this up into St number, street name, and city, state
+    zip_code = models.IntegerField(max_length=5)
+    image_url = #figure it out
+    yelp_url = #figure it out
+    
+#class WeOwn(models.Model):
+    
+    
