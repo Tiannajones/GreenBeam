043f7fca7e827779f8f350a226e43a97b3a8fd69
Tiannajones <77419054+Tiannajones@users.noreply.github.com>
2022-03-11 10:34:44 -0600

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
+'''
+https://github.com/testdrivenio/django-custom-user-model/blob/master/abstract-base-user-example/users/apps.py
+'''
 
-class AppConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'users'
