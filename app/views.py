b2e82fd7400dc785bf7e1e164cebbea2e14d9ed5
Tiannajones <77419054+Tiannajones@users.noreply.github.com>
2022-03-20 18:07:59 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
-#NEW BRANCH
+#https://github.com/testdrivenio/django-custom-user-model/blob/master/abstract-base-user-example/users/views.py
+
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm 
+
+
+class SignUp(generic.CreateView):
+    form_class = CustomUserCreationForm 
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
