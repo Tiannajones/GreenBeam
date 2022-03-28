c232bd3048fb299d208854709b5d52075d90e955
EthanPoe323 <77646568+EthanPoe323@users.noreply.github.com>
2022-03-28 10:31:13 -0500

    Minor Tweaks
    Updates

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import { SearchBar } from 'react-native-elements';
 import { StyleSheet, Text, View } from 'react-native';
 
-import React from 'react';
 
 export default function HomeLoad() {
   return (
     <View style={styles.container}>
+      
+      <SearchBar
+            placeholder="Search Contacts..."
+            placeholderTextColor="darkgrey"
+            containerStyle={style.container_style}
+            inputStyle={{ backgroundColor: "lightgrey", fontSize: 15 }}
+            inputContainerStyle={{
+              backgroundColor: "lightgrey",
+              height: 20,
+            }}
+            onChangeText={(searchTerm) => {
+              console.log(searchTerm);
+            }}
+            value={searchText}
+          />
+
       <Text>Home </Text>
     </View>
   );
