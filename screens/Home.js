ff4d2003c10fdeb7836f198f77d76d74657e0085
EthanPoe323 <77646568+EthanPoe323@users.noreply.github.com>
2022-03-28 10:46:21 -0500

    Map/Search Bar
    working on home page aspects

--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,43 @@
 import React from 'react';
-import { SearchBar } from 'react-native-elements';
-import { StyleSheet, Text, View } from 'react-native';
+// import { SearchBar } from 'react-native-elements';
+// import { StyleSheet, Text, View } from 'react-native';
+import { TouchableHighlight, StyleSheet, Text, View, TextInput, Keyboard  } from 'react-native';
 
 
-export default function HomeLoad() {
+export default function HomeLoad({ navigation }) {
   return (
-    <View style={styles.container}>
-      
-      <SearchBar
-            placeholder="Search Contacts..."
-            placeholderTextColor="darkgrey"
-            containerStyle={style.container_style}
-            inputStyle={{ backgroundColor: "lightgrey", fontSize: 15 }}
-            inputContainerStyle={{
-              backgroundColor: "lightgrey",
-              height: 20,
-            }}
-            onChangeText={(searchTerm) => {
-              console.log(searchTerm);
-            }}
-            value={searchText}
-          />
+    <View style={styles.container}> 
 
-      <Text>Home </Text>
-    </View>
+    <SearchBar
+      placeholder="Search Contacts..."
+      placeholderTextColor="darkgrey"
+      containerStyle={style.container_style}
+      inputStyle={{ backgroundColor: "lightgrey", fontSize: 15 }}
+      inputContainerStyle={{
+        backgroundColor: "lightgrey",
+        height: 20,
+      }}
+      onChangeText={(searchTerm) => {
+        console.log(searchTerm);
+      }}
+        value={searchText}
+      /> 
+
+    <TextInput
+      style={styles.input}
+      onChangeText={onMap}
+      value={Map}
+      placeholder="Dynamic Map"
+    />
+
+<TouchableHighlight onPress={() => mapDisplay(Map)} style={styles.button}>
+            <Text style = {styles.text}>
+               dynamicMap
+            </Text>
+      </TouchableHighlight>
+
+      </View>
+
   );
 }
 
