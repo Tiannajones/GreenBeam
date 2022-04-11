457da2534d1608429be15978985326d98d2e7274
Jcasazza32 <70873956+Jcasazza32@users.noreply.github.com>
2022-04-11 13:01:15 -0500

    Add files via upload

---
+++ screens/style.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View, TextInput,Image, Keyboard  } from 'react-native';
+
+
+export const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#19b194',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  home: {
+    flex: 1,
+    backgroundColor: '#19b194',
+    alignItems: 'center',
+  },
+   button: {
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    margin: 8,
+    height: 40,
+    width: 200,
+  },
+  addrestbutton: {
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    margin: 8,
+    height: 40,
+    width: '45%',
+  },
+  longbutton: {
+    backgroundColor: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    margin: 8,
+    height: 40,
+    width: '85%',
+  },
+  text: {
+     fontSize: 20,
+      borderColor: 'black',
+      fontFamily: 'OleoScript_400Regular',
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 8,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#fff",
+    textAlign: 'center'
+  },
+});
