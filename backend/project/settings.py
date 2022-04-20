4300375f9f9e7a13741726f4d08512332146a913
Carlos Esteban <estebcar29@gmail.com>
2022-04-20 08:20:38 -0500

    completed implementing new users to backend

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,7 +37,7 @@ AUTH_USER_MODEL = "users.NewUser"
 
 INSTALLED_APPS = [
     'corsheaders',
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
