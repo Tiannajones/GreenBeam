d4769737a302fa19706c51c11a4e9a45c1cd232a
Tiannajones <77419054+Tiannajones@users.noreply.github.com>
2022-04-25 09:41:00 -0500

    Update Home.js
    Tianna's changes

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -1,108 +1,111 @@
-import React from 'react';
-// import { SearchBar } from 'react-native-elements';
-//import Geolocation from '@react-native-community/geolocation'; // paste inside map view (for get location) // for request location permission
-import { TouchableHighlight, StyleSheet, Text, View, TextInput, Keyboard  } from 'react-native';
-import { Platform } from 'react-native-web';
+import { TouchableHighlight, StyleSheet, Text, View, TextInput, Keyboard, TouchableOpacity, Alert, ScrollView  } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
 import {styles} from './style.js';
-
+import React, { Component, useState } from 'react';
+import { block } from 'react-native-reanimated';
 export default function HomeLoad({ navigation }) {
- /* 
-  const [Search, onSearch] = React.useState();
-  const items = [...Array(100)].map((val, i) => `Item ${i}`);
-
-  const searchAttempt = (Search) => {
-    if(Boolean(Search)) {
-      console.log(Search);
-      navigation.push("HomeDrawer");
-    }
-  }
-  
-  const App = () => (
-    <div className="Home.js">
-      <div className="leftCol">
-        leftCol
-      </div>
-      
-      <div className="centerCol">
-        <span>List</span>
-        <ul>
-          {items.map((item, i) => (<li key={`item_${i}`}>{ item }</li>))}
-        </ul>
-      </div>
-      
-      <div className="rightCol">
-        rightCol
-      </div>
-    </div>
-  );
-  
-  renderRestaurantBubble = ({item}) => 
-  <View> 
-    <Text>
-      
-    </Text>
-  </View>
-
-
-
-
-
-
-  
-   // ReactDOM.render(<App />, document.getElementById('react'));
-
-
-/*
-<MapView
-         style={mapstyles.map}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
-         initialRegion={{
-         latitude: 30.633263,
-         longitude: -97.677986,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-        />
-  */
+  const [enterGoal, setEnterGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+   const [tmpArray] = useState([
+      // this is the original format
+      // instead needs to be the contents of the
+      // database... most likely implemented by backend
+      { name: "Jimmy Johns", rating: 75, distance: "1.2", categories: "sandwitch", address: "950 W University Ave Ste. 201", url: "www.google.com" },
+      { name: "Panda Express", rating: 75, distance: "1.2", categories: "sandwitch", address: "950 W University Ave Ste. 201", url: "www.google.com" },
+      { name: "Jimmy Johns", rating: 75, distance: "1.2", categories: "sandwitch", address: "950 W University Ave Ste. 201", url: "www.google.com" },
+      { name: "Panda Express", rating: 75, distance: "1.2", categories: "sandwitch", address: "950 W University Ave Ste. 201", url: "www.google.com" }
+    ]);
+  const goalInputHandler = enteredText => {
+    setEnterGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enterGoal]);
+    // setEnterGoal('');
+  };
   return (
-    <View style={styles.container}> 
-
-    <TextInput
+    <View style={styles.container}>
+      <TextInput
       style={styles.input}
-      onChangeText={console.log()}
-      value={console.log()}
+      onChangeText={console.log("big")}
+      value={"Works"}
       placeholder="Searching"
     />
-    
      <View style={{height: 400,
    width: 400, backgroundColor: '#fff'}}>
-        <MapView
-         style={mapstyles.map}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
-         initialRegion={{
-         latitude: 30.633263,
-         longitude: -97.677986,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-        />
       </View>
-    <TouchableHighlight onPress={() => searchAttempt(Search)} style={styles.button}>
+      <TouchableHighlight onPress={() => searchAttempt(Search)} style={styles.button}>
             <Text style = {styles.text}>
                Search
             </Text>
       </TouchableHighlight>
+      <ScrollView>
+        {tmpArray.map(item => (
+          <View style={stylet.listItem}>
+            <Text>
+             {item.rating}
 
-      </View>
+             </Text>
+            <Text key={item}>
+            {item.name}
+             
+            
+           
 
+            {item.distance}
+
+
+            {item.categroies}
+
+
+            {item.address}
+          </Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   );
-}
-const mapstyles = StyleSheet.create({
-  map: {
-    ...StyleSheet.absoluteFillObject,
+};
+const stylet = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputcontainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+  },
+  listItem: {
+    padding: 5,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 4,
+    marginVertical: 2,
+    height:80,
+    width: 215,
+    letterSpacing:1,
+    textDecorationColor: 'red',
+    textShadowColor:'gray',
+   
+    
   },
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
